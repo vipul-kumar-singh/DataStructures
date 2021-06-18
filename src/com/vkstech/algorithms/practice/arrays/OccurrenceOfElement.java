1dc853a2448563d4aa337521124070b5f391c9cf
Vipul Kumar Singh <vipul24.9669@gmail.com>
2021-06-18 16:10:05 +0530

    Occurrence of Element in Array

--- src/com/vkstech/algorithms/practice/arrays/OccurrenceOfElement.java
+++ src/com/vkstech/algorithms/practice/arrays/OccurrenceOfElement.java
@@ -1,4 +1,25 @@
 package com.vkstech.algorithms.practice.arrays;
 
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Find the occurrence of an integer in the array
+ */
 public class OccurrenceOfElement {
+
+    private static int getOccurrence(int[] arr, int x) {
+        Map<Integer, Integer> occurrenceMap = new HashMap<>();
+        for (int val : arr){
+            occurrenceMap.merge(val, 1, Integer::sum);
+        }
+        return occurrenceMap.get(x);
+    }
+
+    public static void main(String[] args) {
+        int[] input = {0, 5, 5, 5, 4};
+        int x = 5;
+
+        System.out.println(getOccurrence(input, x));
+    }
 }
