7b3f89641f5ad2ef91272345092eef6b27acef93
Vipul Kumar Singh <vipul24.9669@gmail.com>
2021-06-30 18:00:37 +0530

    Anagram

---
+++ src/com/vkstech/algorithms/practice/string/Anagram.java
@@ -0,0 +1,28 @@
+package com.vkstech.algorithms.practice.string;
+
+public class Anagram {
+
+    private static boolean isAnagram(String s1, String s2) {
+        if (s1.length() != s2.length())
+            return false;
+
+        int[] count = new int[256];
+
+        for (int i = 0; i < s1.length(); i++) {
+            count[s1.charAt(i) - 'a']++;
+            count[s2.charAt(i) - 'a']--;
+        }
+
+        for (int value : count) {
+            if (value != 0)
+                return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String s1 = "listen";
+        String s2 = "silent";
+        System.out.println(isAnagram(s1, s2));
+    }
+}
