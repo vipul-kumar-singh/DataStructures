9289059c1d12a36364c3f38554d082c00b799d46
Vipul Kumar Singh <vipul24.9669@gmail.com>
2021-07-05 23:31:29 +0530

    Basic LinkedList

---
+++ src/com/vkstech/algorithms/practice/linkedList/LinkedList.java
@@ -0,0 +1,31 @@
+package com.vkstech.algorithms.practice.linkedList;
+
+import java.util.Objects;
+
+public class LinkedList {
+
+    public Node head;
+
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+        }
+    }
+
+    public void insert(int data) {
+        LinkedList.Node node = new LinkedList.Node(data);
+
+        if (Objects.isNull(head)) {
+            head = node;
+        } else {
+            LinkedList.Node start = head;
+            while (Objects.nonNull(start.next)) {
+                start = start.next;
+            }
+            start.next = node;
+        }
+    }
+}
