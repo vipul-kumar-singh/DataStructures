033177518f22910006e13c34b6abcecdec729de8
Vipul Kumar Singh <vipul24.9669@gmail.com>
2021-07-27 22:00:50 +0530

    Binary Tree

---
+++ src/com/vkstech/algorithms/practice/tree/BinaryTree.java
@@ -0,0 +1,17 @@
+package com.vkstech.algorithms.practice.tree;
+
+public class BinaryTree {
+
+    Node root;
+
+    public static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data) {
+            this.data = data;
+        }
+    }
+
+}
