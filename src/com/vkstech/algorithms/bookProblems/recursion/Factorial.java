7f28d3a534ade0deb28be785400e2f570de58e55
Vipul Kumar Singh <vipul24.9669@gmail.com>
2024-04-11 13:55:26 +0530

    Factorial Problem

--- src/com/vkstech/algorithms/bookProblems/recursion/Factorial.java
+++ src/com/vkstech/algorithms/bookProblems/recursion/Factorial.java
@@ -1,2 +1,20 @@
-package com.vkstech.algorithms.bookProblems.recursion;public class Factorial {
+package com.vkstech.algorithms.bookProblems.recursion;
+
+public class Factorial {
+
+    public static int factorial(int n) {
+        if (n == 1) {
+            System.out.println("Returning 1");
+            return 1;
+        } else {
+            System.out.println("Calling factorial(" + (n-1) + ")");
+            int result =  n * factorial(n - 1);
+            System.out.println("Returning resullt: " + result);
+            return result;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(factorial(5));
+    }
 }
