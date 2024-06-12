46bee75827d466e9ff28b3f4a61ec0e3104ecc5c
Vipul Kumar Singh <vipul24.9669@gmail.com>
2024-06-13 00:04:13 +0530

    Delete a node from BST

--- src/com/vkstech/algorithms/practice2/tree/BinarySearchTree.java
+++ src/com/vkstech/algorithms/practice2/tree/BinarySearchTree.java
@@ -32,6 +32,52 @@ public class BinarySearchTree extends BinaryTree {
 
     }
 
+    public void delete(int key) {
+        this.root = delete(root, key);
+    }
+
+    private Node delete(Node node, int key) {
+        if (node == null)
+            return node;
+
+        if (node.data > key) {
+            node.left = delete(node.left, key);
+            return node;
+        }
+
+        if (node.data < key) {
+            node.right = delete(node.right, key);
+            return node;
+        }
+
+        // If left child is empty
+        if (node.left == null) {
+            return node.right;
+        }
+
+        // If right child is empty
+        if (node.right == null) {
+            return node.left;
+        }
+
+        // If both children exists
+        Node parent = node;
+        Node next = node.right;
+
+        while (next.left != null) {
+            parent = next;
+            next = next.left;
+        }
+
+        if (parent != node)
+            parent.left = next.right;
+        else
+            parent.right = next.right;
+
+        node.data = next.data;
+        return node;
+    }
+
     public static void main(String[] args) {
         BinarySearchTree bst = new BinarySearchTree();
         bst.insert(3);
@@ -43,5 +89,13 @@ public class BinarySearchTree extends BinaryTree {
         System.out.println();
         System.out.println(isPresent(bst.root, 2));
         System.out.println(isPresent(bst.root, 5));
+
+        bst.delete(2);
+        BinaryTreeTraversal.inOrder(bst.root);
+        System.out.println();
+
+        bst.delete(3);
+        BinaryTreeTraversal.inOrder(bst.root);
+        System.out.println();
     }
 }
