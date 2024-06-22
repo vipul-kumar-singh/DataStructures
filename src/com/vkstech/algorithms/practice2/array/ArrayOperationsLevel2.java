83c5f12b995e4251bbbd6615e4ed8f9b0fa694c8
Vipul Kumar Singh <vipul24.9669@gmail.com>
2024-06-22 19:44:10 +0530

    Minimize the maximum difference between the heights

--- src/com/vkstech/algorithms/practice2/array/ArrayOperationsLevel2.java
+++ src/com/vkstech/algorithms/practice2/array/ArrayOperationsLevel2.java
@@ -319,6 +319,26 @@ public class ArrayOperationsLevel2 {
         return requiredPlatform;
     }
 
+    public static int getMinDiff(int[] arr, int k) {
+        int n = arr.length;
+        Arrays.sort(arr);
+
+        int ans = arr[n - 1] - arr[0];
+
+        int tempmin, tempmax;
+
+        for (int i = 1; i < n; i++) {
+            if (arr[i] - k < 0)
+                continue;
+
+            tempmin = Math.min(arr[0] + k, arr[i] - k);
+            tempmax = Math.max(arr[i - 1] + k, arr[n - 1] - k);
+
+            ans = Math.min(ans, tempmax - tempmin);
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
 
         int[] arr1 = {1, 2, 3, 4, 5};
@@ -375,6 +395,11 @@ public class ArrayOperationsLevel2 {
         int[] arr = {100, 300, 600};
         int[] dep = {900, 400, 500};
         System.out.println(minPlatform(arr, dep));
+
+        int[] arr17 = {1, 5, 15, 10};
+        int k = 3;
+        System.out.println(getMinDiff(arr17, k));
+
     }
 
 }
