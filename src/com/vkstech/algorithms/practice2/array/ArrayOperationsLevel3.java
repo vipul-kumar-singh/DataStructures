da48cc728971ce421b5b2531eb26f5c3190916db
Vipul Kumar Singh <vipul24.9669@gmail.com>
2024-06-23 22:37:25 +0530

    Maximum Sum Path in Two Arrays

--- src/com/vkstech/algorithms/practice2/array/ArrayOperationsLevel3.java
+++ src/com/vkstech/algorithms/practice2/array/ArrayOperationsLevel3.java
@@ -2,7 +2,7 @@ package com.vkstech.algorithms.practice2.array;
 
 public class ArrayOperationsLevel3 {
 
-    private static int maxIndexDiff(int[] arr) {
+    public static int maxIndexDiff(int[] arr) {
         int n = arr.length;
 
         int[] leftMin = new int[n];
@@ -27,8 +27,52 @@ public class ArrayOperationsLevel3 {
         return maxDist;
     }
 
+    public static int maxSumPath(int[] arr1, int[] arr2) {
+        int i = 0;
+        int j = 0;
+
+        int sum1 = 0;
+        int sum2 = 0;
+        int maxSum = 0;
+
+        while (i < arr1.length && j < arr2.length) {
+            if (arr1[i] < arr2[j]) {
+                sum1 += arr1[i];
+                i++;
+            } else if (arr1[i] > arr2[j]) {
+                sum2 += arr2[j];
+                j++;
+            } else {
+                maxSum += Math.max(sum1, sum2);
+                maxSum += arr1[i];
+                sum1 = 0;
+                sum2 = 0;
+                i++;
+                j++;
+            }
+        }
+
+        while (i < arr1.length) {
+            sum1 += arr1[i];
+            i++;
+        }
+
+        while (j < arr2.length) {
+            sum2 += arr2[j];
+            j++;
+        }
+
+        return maxSum + Math.max(sum1, sum2);
+
+    }
+
     public static void main(String[] args) {
         int[] arr1 = {34, 8, 10, 3, 2, 80, 30, 33, 1};
-        System.out.print(maxIndexDiff(arr1));
+        System.out.println(maxIndexDiff(arr1));
+
+
+        int[] arr2 = {2, 3, 7, 10, 12, 15, 30, 34};
+        int[] arr3 = {1, 5, 7, 8, 10, 15, 16, 19};
+        System.out.println(maxSumPath(arr2, arr3));
     }
 }
