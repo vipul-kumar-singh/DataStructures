2287e77fbe6892298378cd1fc6babee6d8ac927b
Vipul Kumar Singh <vipul24.9669@gmail.com>
2024-06-28 23:00:32 +0530

    Anagram Palindrome

--- src/com/vkstech/algorithms/practice2/string/StringOperations.java
+++ src/com/vkstech/algorithms/practice2/string/StringOperations.java
@@ -46,6 +46,31 @@ public class StringOperations {
         return true;
     }
 
+    public static boolean isAnagramPalindrome(String str) {
+        if (str == null)
+            return false;
+
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : str.toCharArray()) {
+            if (map.containsKey(c))
+                map.put(c, map.get(c) + 1);
+            else
+                map.put(c, 1);
+        }
+
+        int oddCount = 0;
+
+        for (Integer v : map.values()) {
+            if (v % 2 != 0) {
+                oddCount++;
+                if (oddCount > 1)
+                    return false;
+            }
+        }
+
+        return true;
+    }
+
     public static void main(String[] args) {
         String s1 = "abcba";
         System.out.println(isPalindrome(s1));
@@ -53,5 +78,8 @@ public class StringOperations {
         String s2 = "geeksforgeeks";
         String s3 = "forgeeksgeeks";
         System.out.println(isAnagram(s2, s3));
+
+        String s4 = "geeksfogeeks";
+        System.out.println(isAnagramPalindrome(s4));
     }
 }
