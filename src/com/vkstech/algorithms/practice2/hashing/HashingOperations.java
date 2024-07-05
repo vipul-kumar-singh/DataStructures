925e6832e1c4696b269e655a7610c74436e34acd
Vipul Kumar Singh <vipul24.9669@gmail.com>
2024-07-05 15:57:48 +0530

    Check Equal Arrays

--- src/com/vkstech/algorithms/practice2/hashing/HashingOperations.java
+++ src/com/vkstech/algorithms/practice2/hashing/HashingOperations.java
@@ -51,6 +51,33 @@ public class HashingOperations {
         return result;
     }
 
+    public static boolean isEqualArrays(int[] arr1, int[] arr2) {
+        if (arr1.length != arr2.length)
+            return false;
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : arr1) {
+            if (map.containsKey(num))
+                map.put(num, map.get(num) + 1);
+            else
+                map.put(num, 1);
+        }
+
+        for (int num : arr2) {
+            if (!map.containsKey(num))
+                return false;
+            else
+                map.put(num, map.get(num) - 1);
+        }
+
+        for (Integer num : map.values()) {
+            if (num > 0)
+                return false;
+        }
+
+        return true;
+    }
+
     public static void main(String[] args) {
         int[][] mat1 = {{2, 1, 4, 3},
                 {1, 2, 3, 2},
@@ -68,5 +95,9 @@ public class HashingOperations {
         int[] arr1 = {2, 3, 2, 3, 5};
         int[] result = frequencyMatch(arr1);
         System.out.println(Arrays.toString(result));
+
+        int[] arr2 = {1, 2, 5, 4, 0};
+        int[] arr3 = {2, 4, 5, 0, 1};
+        System.out.println(isEqualArrays(arr2, arr3));
     }
 }
