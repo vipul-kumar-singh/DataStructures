2ec19765c7835383dde8749ea5b100c4f5b878dc
Vipul Kumar Singh <vipul24.9669@gmail.com>
2024-10-12 20:57:59 +0530

    Fibonacci using dynamic array

--- src/com/vkstech/algorithms/practice2/dynamicProgramming/FibonacciUtil.java
+++ src/com/vkstech/algorithms/practice2/dynamicProgramming/FibonacciUtil.java
@@ -1,11 +1,20 @@
 package com.vkstech.algorithms.practice2.dynamicProgramming;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class FibonacciUtil {
 
     private final Map<Integer, Integer> map = new HashMap<>();
+
+    private final static int[] arr = new int[100];
+
+    static {
+        Arrays.fill(arr, -1);
+        arr[0] = 0;
+        arr[1] = 1;
+    }
 
     public int fibonacci(int n) {
         if (n <= 1)
@@ -19,8 +28,17 @@ public class FibonacciUtil {
         return result;
     }
 
+    public int getFibonacci(int n) {
+        if (arr[n] != -1)
+            return arr[n];
+
+        arr[n] = getFibonacci(n - 1) + getFibonacci(n - 2);
+        return arr[n];
+    }
+
     public static void main(String[] args) {
         FibonacciUtil fibonacciUtil = new FibonacciUtil();
         System.out.println(fibonacciUtil.fibonacci(10));
+        System.out.println(fibonacciUtil.getFibonacci(10));
     }
 }
